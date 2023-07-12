1d207522d3798efbf98c7887bd237763fe4ba1b1
Mehul Sharma <69448557+Mehul-Sharma-Git@users.noreply.github.com>
2023-07-12 17:17:51 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 function getConfig() {
 	return {
             apikey:
-                process.env.GOOGLE_DRIVE_API_KEY||"AIzaSyAb2TFu5zTkt5XEBn0hx5TW60j7pv0m3tg",
+                process.env.GOOGLE_DRIVE_API_KEY||"",
             clientId:
                 process.env.GOOGLE_DRIVE_CLIENT_ID||"761684470596-iu5u78ibli1aj989ug30lsjpcta8pre3.apps.googleusercontent.com",
             rootFolder:
